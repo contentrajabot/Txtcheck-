6e03eb789986881ac1a44626010eb0795d99b42f
contentrajabot <109162597+contentrajabot@users.noreply.github.com>
2023-05-14 14:47:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,4 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5849984859:AAHTBxoM6A0eMU3yvOpy0eOT5HxitYIKPRA")
-    API_ID = int(os.environ.get("API_ID", "818663"))
-    API_HASH = os.environ.get("API_HASH", "b31cf04c3cc21aa600eca3a991085702")
-    AUTH_USERS = ""
-
-
+    BOT_TOKEN = os.environ.get("
